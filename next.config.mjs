11f1482772a4889962e0c5896d45afabae690c80
k2584459 <k2584459@kcl.ac.uk>
2025-10-30 17:03:56 +0000

    nextconfig change to include image links

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-  images: {
-    unoptimized: true, // ✅ disables Image Optimization for static export
-  },
+const nextConfig = {
+  output: "export", // ensures Next.js generates static HTML
   reactStrictMode: true,
+
+  images: {
+    unoptimized: true, // required for static exports
+  },
+
+  basePath: "/studyplanner-frontend",
+  assetPrefix: "/studyplanner-frontend/",
 };
 
 export default nextConfig;
